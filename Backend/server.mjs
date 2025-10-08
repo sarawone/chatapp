fbd848442876e838ec0e59096795bee5de42da03
sarawone <sarawone.poudal@gmail.com>
2025-10-08 22:34:27 +0100

    adding websocket on backend

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import fs from "fs";
 import { timeStamp } from "console";
-
+import {server as WebSocketServer} from "websocket"; // importing the websocket
+import http from "http";
 const app = express();
 const PORT =  3000;
+
+const server = http.createServer(app);   // creating http server
+const wsServer = new WebSocketServer ({httpServer:server}); 
 
 app.use(cors());
 app.use(express.json());
@@ -40,4 +44,38 @@ app.post("/messages",(req,res)=>{
 
 });
 
-app.listen(PORT,() => console.log("Server running on http://localhost:3000"));
+//app.listen(PORT,() => console.log("Server running on http://localhost:3000"));
+
+//  webSocket handling
+
+wsServer.on("request",request =>{
+    const connection = request.accept(null);
+    console.log("New WebSocket connection");
+
+    //load history message 
+    connection.sendUTF(JSON.stringify({type : "history",message :loadMessages()}));
+
+    connection.on("message", message => {
+        const data = JSON.parse(message);
+
+        const {text ,user}= data;
+        const messages = loadMessages();
+        const newMsg = {text,user,timeStamp: new Date().toISOString()};
+        messages.push(newMsg);
+        saveMessages(messages);
+
+        //brodcast to all connect user
+
+        wsServer.connections.forEach(conn => {
+            conn.sendUTF(JSON.stringify({type: "newMessage", message :newMsg}));
+        });
+
+    });
+
+    connection.on("close",() => console.log("Disconnected..."));
+
+});
+
+server.listen(PORT,() =>{
+    console.log("Server running on http://localhost:3000");
+});
