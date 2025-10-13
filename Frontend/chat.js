3afec5ece0e5f54c790adae33e2bf91b5451be78
sarawone <sarawone.poudal@gmail.com>
2025-10-13 11:03:00 +0100

    adding code for websocket

--- Frontend/chat.js
+++ Frontend/chat.js
@@ -4,8 +4,38 @@ const sent = document.getElementById("sentBtn");
 
 const API_URL = "http://localhost:3000/messages";
 
-//load previous message from backend
+// webscoket connection
 
+const ws = new WebSocket("ws://localhost:3000");
+
+ws.onopen = () => {
+    console.log("Connected to websocket server");
+}
+ws.onmessage = (event) =>{
+    const data = JSON.parse(event.data);
+
+    if (data.type == "history")
+    {
+        messages.innerHTML = "";
+        data.messages.forEach(msg => addMessage(msg));
+    }
+    else if (data.type == "newMessage")
+    {
+        addMessage(data.message);
+    }
+}
+ws.onclose = () => {
+    console.log("Disconnected from websocket server");
+}
+
+function addMessage(msg){
+    const div = document.createElement("div");
+    div.textContent = `${msg.user} : ${msg.text}`;
+    messages.appendChild(div);
+}
+
+//load previous message from backend (http)
+/*
 async function loadMessages()
 {
     const res = await fetch(API_URL);
@@ -17,6 +47,7 @@ async function loadMessages()
        messages.appendChild(div);
     });
 }
+    */
 
 
 //To show sent message
@@ -26,17 +57,23 @@ async function sendMessage()
     const text = input.value.trim();
     if (!text) return;
 
+    const message = {text,user: "You"};
+    ws.send(JSON.stringify(message));
+
+    /*
     await fetch(API_URL,{
         method : "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({text, user: "You"})
     });
+    */
+
 
     input.value = "";
-    loadMessages();
+    //loadMessages();
 
 }
 
 sent.addEventListener("click",sendMessage);
 
-loadMessages();
+//loadMessages();
